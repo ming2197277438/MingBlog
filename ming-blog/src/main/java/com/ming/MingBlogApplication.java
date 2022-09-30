f365b40ba62b55eff5142d9bc644b5ddcd49e1dc
sunming <2197277438@qq.com>
2022-09-30 14:18:35 +0800

    配置文件

--- ming-blog/src/main/java/com/ming/MingBlogApplication.java
+++ ming-blog/src/main/java/com/ming/MingBlogApplication.java
@@ -1,10 +1,11 @@
 package com.ming;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MingBlogApplication {
     public static void main(String[] args) {
-        
+        SpringApplication.run(MingBlogApplication.class, args);
     }
 }
