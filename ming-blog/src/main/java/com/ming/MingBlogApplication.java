61bbcf17d0885f2529c5cfba7219680e6b018885
sunming <2197277438@qq.com>
2022-09-30 14:03:09 +0800

    前端启动类

--- ming-blog/src/main/java/com/ming/MingBlogApplication.java
+++ ming-blog/src/main/java/com/ming/MingBlogApplication.java
@@ -1,4 +1,10 @@
 package com.ming;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MingBlogApplication {
+    public static void main(String[] args) {
+        
+    }
 }
